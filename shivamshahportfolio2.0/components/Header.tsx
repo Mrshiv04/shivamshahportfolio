7979197f1bfa8146ee1c29a91d487bdf47915e9d
Shivam Shah <shiva.sean27@gmail.com>
2022-11-10 02:09:31 +0530

    after adding hero section

--- shivamshahportfolio2.0/components/Header.tsx
+++ shivamshahportfolio2.0/components/Header.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
 
 type Props = {};
 
 function Header({}: Props) {
 	return (
 		<header className='sticky top-0 p-5 flex items-start justify-between max-w-5xl mx-auto z-20 xl:items-center'>
-			<div className='flex flex-row items-center'>
+			<motion.div
+				initial={{
+					x: -500,
+					opacity: 0,
+					scale: 0.5,
+				}}
+				animate={{
+					x: 0,
+					opacity: 1,
+					scale: 1,
+				}}
+				transition={{
+					duration: 1.5,
+				}}
+				className='flex flex-row items-center'
+			>
 				<SocialIcon
 					url='https://www.linkedin.com/in/shivam-shah-924737117/'
 					fgColor='gray'
@@ -34,9 +50,24 @@ function Header({}: Props) {
 					bgColor='transparent'
 					target='_blank'
 				/>
-			</div>
+			</motion.div>
 
-			<div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+			<motion.div
+				initial={{
+					x: 500,
+					opacity: 0,
+					scale: 0.5,
+				}}
+				animate={{
+					x: 0,
+					opacity: 1,
+					scale: 1,
+				}}
+				transition={{
+					duration: 1.5,
+				}}
+				className='flex flex-row items-center text-gray-300 cursor-pointer'
+			>
 				<SocialIcon
 					className='cursor-pointer'
 					network='email'
@@ -46,7 +77,7 @@ function Header({}: Props) {
 				<p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
 					Get in touch
 				</p>
-			</div>
+			</motion.div>
 		</header>
 	);
 }
--- shivamshahportfolio2.0/pages/index.tsx
+++ shivamshahportfolio2.0/pages/index.tsx
@@ -1,17 +1,19 @@
 import Head from 'next/head';
 import Header from '../components/Header';
-import styles from '../styles/Home.module.css';
+import Hero from '../components/Hero';
 
 export default function Home() {
 	return (
-		<div>
+		<div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
 			<Head>
 				<title>Shivam Shah 2.0</title>
 			</Head>
 
 			<Header />
-			{/* Header */}
 
+			<section id='hero' className='snap-center'>
+				<Hero />
+			</section>
 			{/* Hero */}
 
 			{/* About */}
