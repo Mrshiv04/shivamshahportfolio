ec0a814a6f71d010548ea0523e367d2d2b40f91e
Shivam Shah <shiva.sean27@gmail.com>
2022-12-28 16:48:28 +0530

    after adding project details

--- shivamshahportfolio2.0/components/Project.tsx
+++ shivamshahportfolio2.0/components/Project.tsx
@@ -4,7 +4,71 @@ import { motion } from 'framer-motion';
 type Props = {};
 
 function Project({}: Props) {
-	const projects = [1, 2, 3, 4, 5];
+	const projects = [
+		{
+			name: 'Netflix Clone',
+			source:
+				'https://www.passionateinmarketing.com/wp-content/uploads/2022/08/netflix.jpg',
+			details:
+				'Created a Netflix clone where contents are changed dynamic ally every time you refresh or open the application. The application looks almost same as that of the original Netflix application.',
+			link: 'https://netflix-clone-ac8bc.web.app/',
+		},
+		{
+			name: 'Google Search Clone',
+			source:
+				'https://cdn.dribbble.com/users/2037940/screenshots/6228108/google-logo.png',
+			details:
+				'Created a Google Search Clone using React.Js where user can search a keyword and the application will return multiple results to that keyword.',
+			link: 'https://clone-4b426.web.app/',
+		},
+		{
+			name: 'LinkedIn Clone',
+			source:
+				'https://cdn.searchenginejournal.com/wp-content/uploads/2022/08/linkedin-search-630834c87e29b-sej-1520x800.webp',
+			details:
+				'Created a Linkedin clone having functionality of posting in feed, login and logout, and even creating a new account with profile pictures.',
+			link: 'https://shivam-linkedin-clone.web.app/',
+		},
+		{
+			name: 'Amazon Clone',
+			source:
+				'https://duet-cdn.vox-cdn.com/thumbor/0x0:2040x1360/828x552/filters:focal(1020x680:1021x681):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/13443481/acastro_181114_1777_amazon_hq2_0006.jpg',
+			details:
+				'Created an Amazon website clone having functionality of selecting items from catalogue, checking out with payment window , login and logout, and even creating a new account.',
+			link: 'https://shivam-amazn-clone.web.app/',
+		},
+		{
+			name: 'AirBnB Clone',
+			source: 'https://icaninfotech.com/wp-content/uploads/2021/06/Airbnb.png',
+			details:
+				'Created an AirBnB replica using react.JS , material UI, HTML/CSS which has the exact look and feel of the original website.',
+			link: 'https://shivam-airbnb.web.app/',
+		},
+		{
+			name: 'Gmail Clone',
+			source:
+				'https://i.pcmag.com/imagery/articles/025Oaztt0ita5uBZZnF7f6p-1.fit_lim.size_1600x900.v1662056326.jpg',
+			details:
+				'Created an Gmail replica using react.Js , materialUI, HTML/CSS which has the exact look and feel of the original website.',
+			link: 'https://shivam-mail-clone.web.app/',
+		},
+		{
+			name: 'Bingewatching Database',
+			source:
+				'https://images.creativemarket.com/0.1.0/ps/7414066/600/400/m2/fpnw/wm1/logo-design-for-movie-production-company-01-.jpg?1575502358&s=50e3d37c1ab493df98baf6eb75f2a430&fmt=webp',
+			details:
+				'Bingewatching Database is an authoritative source for movie content, designed to help fans explore the world of movies and decide what to watch. Its searchable database includes millions of movies.',
+			link: 'https://shivam-moviesdb.web.app/',
+		},
+		{
+			name: 'Youtube Clone',
+			source:
+				'https://duet-cdn.vox-cdn.com/thumbor/0x0:2040x1360/828x552/filters:focal(1020x680:1021x681):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/10581501/wjoel_1777_180403_youtube_003.jpg',
+			details:
+				'YouTube is an American online video-sharing platform headquartered in San Bruno, California. This is a front-end only application made by me, here ive tried replicating the dark mode of the application. It is slick and almost identical to that of real application.',
+			link: 'https://shivam-utube.web.app/',
+		},
+	];
 	return (
 		<motion.div
 			initial={{
@@ -36,7 +100,7 @@ function Project({}: Props) {
 							}}
 							transition={{ duration: 1.2 }}
 							className='h-40 object-contain'
-							src='https://www.passionateinmarketing.com/wp-content/uploads/2022/08/netflix.jpg'
+							src={project.source}
 							alt='img'
 						/>
 						<div className='space-y-5 md:px-10 max-w-6xl '>
@@ -44,14 +108,20 @@ function Project({}: Props) {
 								<span className='underline decoration-[#F7AB0A]/50'>
 									Case Study {i + 1} of {projects.length}:
 								</span>{' '}
-								Netflix Clone
+								{project.name}
 							</h4>
 							<p className='text-lg text-center md:text-left'>
-								Lorem Ipsum is simply dummy text of the printing and typesetting
-								industry. Lorem Ipsum has been the industry's standard dummy
-								text ever since the 1500s, when an unknown printer took a galley
-								of type and scrambled it to make a type specimen book. It has
-								survived not only five centuries,
+								{project.details}
+							</p>
+							<p>
+								Check out this app here:{' '}
+								<a
+									className='underline decoration-[#F7AB0A]/50'
+									href={project.link}
+									target='_blank'
+								>
+									{project.link}
+								</a>
 							</p>
 						</div>
 					</div>
