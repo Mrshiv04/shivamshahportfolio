dd25ad2e4ce0de7455326a90a9ee9cce7009505d
Shivam Shah <shiva.sean27@gmail.com>
2023-01-02 13:59:10 +0530

    changing sequence of experience

--- shivamshahportfolio2.0/components/Experience.tsx
+++ shivamshahportfolio2.0/components/Experience.tsx
@@ -81,9 +81,9 @@ function Experience({}: Props) {
 				Experience
 			</h3>
 			<div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thin scrollbar-thumb-[#F7AB0A]/80'>
-				<ExperienceCard {...obj1} />
-				<ExperienceCard {...obj2} />
 				<ExperienceCard {...obj3} />
+				<ExperienceCard {...obj2} />
+				<ExperienceCard {...obj1} />
 			</div>
 		</motion.div>
 	);
